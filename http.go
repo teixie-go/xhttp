20a5a5fdb76be7f308d3f1e405f5bd9dcd37b0a8
zusong <419894552@qq.com>
2021-08-17 16:27:32 +0800

    update:重构

--- http.go
+++ http.go
@@ -1,73 +1,140 @@
 package xhttp
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
-	"net/url"
+	"strings"
+	"time"
 )
 
-func resolveClient(client ...*http.Client) *http.Client {
-	if len(client) > 0 {
-		return client[0]
-	}
-	return http.DefaultClient
+var (
+	// DefaultClient is the default Client
+	// and is used by Get, Post, PostForm, and PostJSON.
+	DefaultClient = &Client{client: http.DefaultClient}
+
+	// Global listeners
+	_listeners = make([]ListenerFunc, 0)
+)
+
+//------------------------------------------------------------------------------
+
+type Response struct {
+	err      error
+	result   []byte
+	Request  *http.Request
+	Response *http.Response
+	Duration time.Duration
 }
 
-func Get(uri string, client ...*http.Client) ([]byte, error) {
-	c := resolveClient(client...)
-	response, err := c.Get(uri)
-	if err != nil {
-		return nil, err
+func (r *Response) Bind(o interface{}) error {
+	if r.err != nil {
+		return r.err
 	}
-	defer response.Body.Close()
-	if response.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("http get error : uri=%v , statusCode=%v", uri, response.StatusCode)
-	}
-	return ioutil.ReadAll(response.Body)
+	return json.Unmarshal(r.result, o)
 }
 
-func Post(uri string, data string, client ...*http.Client) ([]byte, error) {
-	c := resolveClient(client...)
-	response, err := c.Post(uri, "", bytes.NewBuffer([]byte(data)))
-	if err != nil {
-		return nil, err
-	}
-	defer response.Body.Close()
-	if response.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("http get error : uri=%v , statusCode=%v", uri, response.StatusCode)
-	}
-	return ioutil.ReadAll(response.Body)
+func (r *Response) Result() ([]byte, error) {
+	return r.result, r.err
 }
 
-func PostForm(uri string, data url.Values, client ...*http.Client) ([]byte, error) {
-	c := resolveClient(client...)
-	response, err := c.PostForm(uri, data)
-	if err != nil {
-		return nil, err
-	}
-	defer response.Body.Close()
-	if response.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("http get error : uri=%v , statusCode=%v", uri, response.StatusCode)
-	}
-	return ioutil.ReadAll(response.Body)
+//------------------------------------------------------------------------------
+
+type ListenerFunc func(resp *Response)
+
+// Add global listeners
+func Listen(listeners ...ListenerFunc) {
+	_listeners = append(_listeners, listeners...)
 }
 
-func PostJSON(uri string, obj interface{}, client ...*http.Client) ([]byte, error) {
-	jsonData, err := json.Marshal(obj)
+//------------------------------------------------------------------------------
+
+type Header map[string]string
+
+type Options struct {
+	Header Header
+}
+
+type Client struct {
+	client *http.Client
+}
+
+func (c *Client) Request(method, url string, body io.Reader, options *Options) *Response {
+	resp := new(Response)
+	defer func() {
+		// dispatch listeners
+		for _, listener := range _listeners {
+			listener(resp)
+		}
+	}()
+	req, err := http.NewRequest(method, url, body)
 	if err != nil {
-		return nil, err
+		resp.err = err
+		return resp
 	}
-	c := resolveClient(client...)
-	response, err := c.Post(uri, "application/json;charset=utf-8", bytes.NewBuffer(jsonData))
+	if options != nil {
+		for k, v := range options.Header {
+			req.Header.Set(k, v)
+		}
+	}
+	resp.Request = req
+	beginTime := time.Now()
+	res, err := c.client.Do(req)
+	resp.Duration = time.Now().Sub(beginTime)
 	if err != nil {
-		return nil, err
+		resp.err = err
+		return resp
 	}
-	defer response.Body.Close()
-	if response.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("http get error : uri=%v , statusCode=%v", uri, response.StatusCode)
+	defer res.Body.Close()
+	resp.Response = res
+	result, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		resp.err = err
+		return resp
 	}
-	return ioutil.ReadAll(response.Body)
+	resp.result = result
+	return resp
+}
+
+func (c *Client) Get(url string) *Response {
+	return c.Request("GET", url, nil, nil)
+}
+
+func (c *Client) Post(url, data string) *Response {
+	return c.Request("POST", url, strings.NewReader(data), nil)
+}
+
+func (c *Client) PostForm(url string, data string) *Response {
+	return c.Request("POST", url, strings.NewReader(data), &Options{
+		Header: Header{"Content-Type": "application/x-www-form-urlencoded"},
+	})
+}
+
+func (c *Client) PostJSON(url string, data string) *Response {
+	return c.Request("POST", url, strings.NewReader(data), &Options{
+		Header: Header{"Content-Type": "application/json;charset=utf-8"},
+	})
+}
+
+//------------------------------------------------------------------------------
+
+func Request(method, url string, body io.Reader, options *Options) *Response {
+	return DefaultClient.Request(method, url, body, options)
+}
+
+func Get(url string) *Response {
+	return DefaultClient.Get(url)
+}
+
+func Post(url, data string) *Response {
+	return DefaultClient.Post(url, data)
+}
+
+func PostForm(url, data string) *Response {
+	return DefaultClient.PostForm(url, data)
+}
+
+func PostJSON(url, data string) *Response {
+	return DefaultClient.PostJSON(url, data)
 }
